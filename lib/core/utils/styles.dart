4b49ca007be9ebcbe6aa1e3e06a320901e66a68a
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-05-28 22:44:54 +0300

    chore: Add Styles class for consistent text styles

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static TextStyle style25 = const TextStyle(
+    fontSize: 25,
+    color: Colors.black,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+  static TextStyle style18 = const TextStyle(
+    fontSize: 18,
+    color: Colors.black,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+  static TextStyle styleBold18 = const TextStyle(
+    fontSize: 18,
+    color: Colors.black,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+  static TextStyle style24 = const TextStyle(
+    fontSize: 24,
+    color: Colors.black,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+}
--- lib/features/checkout/ui/my_cart_view.dart
+++ lib/features/checkout/ui/my_cart_view.dart
@@ -5,6 +5,15 @@ class MyCartView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('My Cart'),
+      ),
+      body: const Center(
+        child: Text('My Cart Page'),
+      ),
+    
+    );
   }
 }
+ 
