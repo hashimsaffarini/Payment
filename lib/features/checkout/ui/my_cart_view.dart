914c8694a20a89046c0de650627888678cdb7877
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-05-28 22:59:04 +0300

    chore: Add new SVG images for arrow

--- lib/features/checkout/ui/my_cart_view.dart
+++ lib/features/checkout/ui/my_cart_view.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:payment/core/utils/app_images.dart';
+import 'package:payment/core/utils/styles.dart';
 
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: const Text('My Cart'),
+        leading: Center(
+          child: SvgPicture.asset(
+            Assets.imagesArrow,
+          ),
+        ),
+        centerTitle: true,
+        title: const Text(
+          'My Cart',
+          style: Styles.style25,
+          textAlign: TextAlign.center,
+        ),
       ),
       body: const Center(
         child: Text('My Cart Page'),
       ),
-    
     );
   }
 }
- 
