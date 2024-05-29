b9dacd1c730bd9bbdec67ba407957e1acc403461
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-05-29 14:25:23 +0300

    feat: Add CustomCreditCard widget for displaying custom credit card details

---
+++ lib/features/checkout/ui/widgets/custom_credit_card.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+
+class CustomCreditCard extends StatefulWidget {
+  const CustomCreditCard({
+    super.key,
+  });
+
+  @override
+  State<CustomCreditCard> createState() => _CustomCreditCardState();
+}
+
+class _CustomCreditCardState extends State<CustomCreditCard> {
+  String cardNumber = '';
+  String expiryDate = '';
+  String cardHolderName = '';
+  String cvvCode = '';
+  bool showBackView = false;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CreditCardWidget(
+          cardNumber: cardNumber,
+          expiryDate: expiryDate,
+          cardHolderName: cardHolderName,
+          cvvCode: cvvCode,
+          showBackView: showBackView,
+          isHolderNameVisible: true,
+          onCreditCardWidgetChange: (value) {},
+        ),
+        CreditCardForm(
+          cardNumber: cardNumber,
+          expiryDate: expiryDate,
+          cardHolderName: cardHolderName,
+          cvvCode: cvvCode,
+          onCreditCardModelChange: (CreditCardModel creditCardModel) {
+            setState(() {
+              cardNumber = creditCardModel.cardNumber;
+              expiryDate = creditCardModel.expiryDate;
+              cardHolderName = creditCardModel.cardHolderName;
+              cvvCode = creditCardModel.cvvCode;
+              showBackView = creditCardModel.isCvvFocused;
+            });
+          },
+          formKey: formKey,
+        ),
+      ],
+    );
+  }
+}
--- lib/features/checkout/ui/widgets/payment_details_view_body.dart
+++ lib/features/checkout/ui/widgets/payment_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payment/features/checkout/ui/widgets/custom_credit_card.dart';
 import 'package:payment/features/checkout/ui/widgets/payment_methods_list_view.dart';
 
 class PaymentDetailsViewBody extends StatelessWidget {
@@ -6,11 +7,15 @@ class PaymentDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        SizedBox(height: 18),
-        PaymentMethods(),
-      ],
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(height: 18),
+          PaymentMethods(),
+          SizedBox(height: 18),
+          CustomCreditCard(),
+        ],
+      ),
     );
   }
 }
