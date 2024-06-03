63014a2b0c20501a9158df84df156bc58aa83dad
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-06-03 15:05:14 +0300

    feat: Add flutter_paypal_payment dependency
    Add the `flutter_paypal_payment` dependency to the `pubspec.yaml` file. This package will enable PayPal payment integration in the app.

--- lib/features/checkout/ui/widgets/custom_button_bloc_consumer.dart
+++ lib/features/checkout/ui/widgets/custom_button_bloc_consumer.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 import 'package:payment/core/widgets/custom_button.dart';
 import 'package:payment/features/checkout/data/models/payment_iintent_input_model.dart';
 import 'package:payment/features/checkout/logic/cubit/payment_cubit.dart';
@@ -29,13 +32,67 @@ class CustomButtonBlocConcumer extends StatelessWidget {
       builder: (context, state) {
         return CustomButton(
           onTap: () {
-            BlocProvider.of<PaymentCubit>(context).makePayment(
-              paymentIintentInputModel: PaymentIintentInputModel(
-                currency: 'USD',
-                amount: '1000',
-                customerID: 'cus_QDmxtEWAjjxGdy',
+            // BlocProvider.of<PaymentCubit>(context).makePayment(
+            //   paymentIintentInputModel: PaymentIintentInputModel(
+            //     currency: 'USD',
+            //     amount: '1000',
+            //     customerID: 'cus_QDmxtEWAjjxGdy',
+            //   ),
+            // );
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => PaypalCheckoutView(
+                sandboxMode: true,
+                clientId: "YOUR CLIENT ID",
+                secretKey: "YOUR SECRET KEY",
+                transactions: const [
+                  {
+                    "amount": {
+                      "total": '100',
+                      "currency": "USD",
+                      "details": {
+                        "subtotal": '100',
+                        "shipping": '0',
+                        "shipping_discount": 0
+                      }
+                    },
+                    "description": "The payment transaction description.",
+                    // "payment_options": {
+                    //   "allowed_payment_method":
+                    //       "INSTANT_FUNDING_SOURCE"
+                    // },
+                    "item_list": {
+                      "items": [
+                        {
+                          "name": "Apple",
+                          "quantity": 4,
+                          "price": '10',
+                          "currency": "USD"
+                        },
+                        {
+                          "name": "Pineapple",
+                          "quantity": 5,
+                          "price": '12',
+                          "currency": "USD"
+                        }
+                      ]
+                    }
+                  }
+                ],
+                note: "Contact us for any questions on your order.",
+                onSuccess: (Map params) async {
+                  log("onSuccess: $params");
+                  Navigator.pop(context);
+                },
+                onError: (error) {
+                  log("onError: $error");
+                  Navigator.pop(context);
+                },
+                onCancel: () {
+                  print('cancelled:');
+                  Navigator.pop(context);
+                },
               ),
-            );
+            ));
           },
           title: 'Continue Payment',
           isLoading: state is PaymentLoading,
